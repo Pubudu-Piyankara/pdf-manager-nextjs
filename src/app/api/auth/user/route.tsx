391663a547fadb091302f3ff1d7945a12b070193
Piyankara N W M G P <pubudupiyankara.me@gmail.com>
2024-06-12 21:41:10 +0530

    authentication

--- src/app/api/auth/user/route.tsx
+++ src/app/api/auth/user/route.tsx
@@ -8,16 +8,16 @@ import { getDataFromToken } from "@/services/getDataFromToken";
 // Ensure the database connection is established
 connectDB();
 
-export async function GET(request: NextRequest) {
-  try {
-    const userId = await getDataFromToken(request);
-    const user = await User.findOne({ _id: userId }).select("-password");
-    return NextResponse.json({
-        message:"User found",
-        data:user
-    })
-  } catch (error: any) {
-    handleError(error);
-    return NextResponse.json({ error: error.message }, { status: 400 });
-  }
-}
+// export async function GET(request: NextRequest) {
+//   try {
+//     const userId = await getDataFromToken(request);
+//     const user = await User.findOne({ _id: userId }).select("-password");
+//     return NextResponse.json({
+//         message:"User found",
+//         data:user
+//     })
+//   } catch (error: any) {
+//     handleError(error);
+//     return NextResponse.json({ error: error.message }, { status: 400 });
+//   }
+// }
